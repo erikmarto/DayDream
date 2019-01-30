53caa8f592bfafea5c1faaaeac3d2509c34eeb1d
Alex Aguilar <aaguilar2@iesfuentesanluis.org>
2019-01-30 13:15:07 +0100

    Cambios en solAquiler y main

--- daydreams/js/main.js
+++ daydreams/js/main.js
@@ -1,24 +1,12 @@
-/* var app = angular.module("app",[]);
-
-app.controller("metodoPago", function($scope){
-    $scope.mensaje = "Metodo de pagodfdhfhhhte";
-});
-
-var metodoPago={
-    tarjeta = false,
-    paypal = false,
-  } */
 
 $('#metodoPago').on('change', function () {
     $('.formulario').hide();
     $('#' + this.value).show();
 });
 
-/* $('.pago').hide();
-$('.pagar').click(function (e){
-    $('.pago').show();
-    e.preventDefault();
-}); */
+var metodo = $('#metodoPago').filter(":selected").val();
+//if($('select[option="paypal"]').on('change')
+console.log(metodo);
 
 $('input[type="submit"]').click(function (e) {
     let precio = "";
@@ -30,20 +18,25 @@ $('input[type="submit"]').click(function (e) {
         $('#productoCarrito').replaceWith('<h6 class="my-0" id="productoCarrito">Has seleccionado el pack fiesta ' + producto + '.</h6>');
         $('#descProducto').replaceWith('<div id="descProducto"><ul class="list-unstyled mt-3 mb-4"><li>PACK DE ALTAVOCES</li><li>REPRODUCTOR</li></ul></div>');
         $('.precioCarrito').replaceWith('<strong class="precioCarrito">'+ precio + ' €</strong>');
+        $('#opcionesPaypal > option[value="Pack Bronce"]').attr('selected', 'selected');
+        $('.pagar').removeAttr("disabled");
         break;
         case 'plata':
         precio = "199";
         $('#productoCarrito').replaceWith('<h6 class="my-0" id="productoCarrito">Has seleccionado el pack fiesta ' + producto + '.</h6>');
         $('#descProducto').replaceWith('<div id="descProducto"><ul class="list-unstyled mt-3 mb-4"><li>PACK DE ALTAVOCES</li><li>REPRODUCTOR</li><li>EFECTOS DE ILUMINACIÓN</li></ul></div>');
         $('.precioCarrito').replaceWith('<strong class="precioCarrito">'+ precio + ' €</strong>');
+        $('#opcionesPaypal > option[value="Pack Plata"]').attr('selected', 'selected');
+        $('.pagar').removeAttr("disabled");
         break;
         case 'oro':
         precio = "380";
         $('#productoCarrito').replaceWith('<h6 class="my-0" id="productoCarrito">Has seleccionado el pack fiesta ' + producto + '.</h6>');
         $('#descProducto').replaceWith('<div id="descProducto"><ul class="list-unstyled mt-3 mb-4"><li>PACK DE ALTAVOCES</li><li>REPRODUCTOR DJ</li><li>EFECTOS DE ILUMINACIÓN</li><li>MÁQUINA DE HUMO</li></ul></div>');
         $('.precioCarrito').replaceWith('<strong class="precioCarrito">'+ precio + ' €</strong>');
+        $('#opcionesPaypal > option[value="Pack Oro"]').attr('selected', 'selected');
+        $('.pagar').removeAttr("disabled");
         break;
-        
     }
     e.preventDefault();
 });
